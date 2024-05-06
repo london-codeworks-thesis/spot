6685fbbb873883a6071f7774dcfb4e43e4c9a2ca
Sunny Anter <sunny@veskim.com>
2024-05-06 18:46:47 +0100

    feat: added inputs and labels to login page

--- app/(loginRoutes)/login/page.tsx
+++ app/(loginRoutes)/login/page.tsx
@@ -1,14 +1,33 @@
-import Link from 'next/link';
 import React from 'react';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 
 export default function Login () {
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      <h1>login page</h1>
-      <Link href='/dashboard'>
-        <Button>login</Button>
-      </Link>
+    <main className='flex min-h-screen flex-col items-center'>
+      <div className='flex h-[70v] w-4/5 flex-col items-start justify-center gap-8 pt-20'>
+        <h1 className='mb-4 text-3xl font-extrabold'>Log In</h1>
+        <div className='flex w-full flex-col gap-3'>
+          <Label>Email address</Label>
+          <Input
+            type='email'
+            placeholder='johndoe@gmail.com'
+            className='h-12'
+          />
+        </div>
+        <div className='flex w-full flex-col gap-3'>
+          <Label>Password</Label>
+          <Input type='password' placeholder='•••••••••••' className='h-12' />
+          <div className='flex w-full justify-end'>
+            <Link href='/forgottenPassword'>
+              <p className='text-xs'>Forgot password?</p>
+            </Link>
+          </div>
+        </div>
+        <Button className='h-12 w-full'>Log In</Button>
+      </div>
     </main>
   );
 }
