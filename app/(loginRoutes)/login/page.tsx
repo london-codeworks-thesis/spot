24a363f13234be1a0a1406635a11028d2d54545d
Sam Polge <sampolge1@gmail.com>
2024-05-06 18:52:56 +0100

    feat: styled register page

--- app/(loginRoutes)/login/page.tsx
+++ app/(loginRoutes)/login/page.tsx
@@ -4,9 +4,9 @@ import { Button } from '@/components/ui/button';
 
 export default function Login () {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       <h1>login page</h1>
-      <Link href="/dashboard">
+      <Link href='/dashboard'>
         <Button>login</Button>
       </Link>
     </main>
