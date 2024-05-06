56566078b234a9aedc003dbc3bd0915c4295dc17
Sam Polge <sampolge1@gmail.com>
2024-05-06 11:15:18 +0100

    Feat/initial routing (#4)
    * feat: initial dashboard folder layout
    
    * feat: login routes

---
+++ app/loginRoutes/register/page.tsx
@@ -0,0 +1,9 @@
+export default function Register() {
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+      <h1>
+        Register page
+      </h1>
+    </main>
+  );
+}
