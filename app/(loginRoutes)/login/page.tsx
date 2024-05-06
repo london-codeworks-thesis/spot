d771d385b560aa97ee6e9aa545330fc554c14ef0
Sunny Anter <sunny@veskim.com>
2024-05-06 19:04:09 +0100

    feat: finished styling for login-page

--- app/(loginRoutes)/login/page.tsx
+++ app/(loginRoutes)/login/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
+import { Separator } from '@/components/ui/separator';
+import { FaApple, FaFacebook, FaGoogle } from 'react-icons/fa';
 import Link from 'next/link';
 
 export default function Login () {
@@ -27,6 +29,30 @@ export default function Login () {
           </div>
         </div>
         <Button className='h-12 w-full'>Log In</Button>
+        <div className='flex w-full items-center justify-center gap-3'>
+          <Separator className='w-4/12' />
+          <p className='text-xs text-gray-400'>Or Login with</p>
+          <Separator className='w-4/12' />
+        </div>
+        <div className='flex w-full flex-row justify-center gap-4'>
+          <Button variant='outline' className='flex-1 py-6'>
+            <FaGoogle />
+          </Button>
+          <Button variant='outline' className='flex-1 py-6'>
+            <FaFacebook />
+          </Button>
+          <Button variant='outline' className='flex-1 py-6'>
+            <FaApple />
+          </Button>
+        </div>
+      </div>
+      <div className='flex h-[30vh] items-end justify-end pb-8'>
+        <p className='text-center text-xs'>
+          Don&apos;t have an account?
+          <Link href='/register'>
+            <span className='font-extrabold'>&nbsp;Sign up</span>
+          </Link>
+        </p>
       </div>
     </main>
   );
