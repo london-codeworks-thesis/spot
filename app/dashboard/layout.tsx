70136245080a32bcd01279fc04c594b9351de4a3
Sunny <153813638+SunnyAnter@users.noreply.github.com>
2024-05-07 09:56:59 +0100

    Feat/nav bar (#14)
    * feat: main navbar icons and buttons added
    
    * feat: added more functionality to navbar & installed drawer from shadcn

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import NavBar from '@/components/nav-bar';
 
 export default function Layout ({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-      <div className='w-full flex-none md:w-64' />
       <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+      <NavBar />
     </div>
   );
 }
