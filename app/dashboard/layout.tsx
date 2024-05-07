373832cace082f79bb6d0d58e91299edf3ed8e32
Sam Polge <sampolge1@gmail.com>
2024-05-07 17:38:59 +0100

    feat: basic map layout and pointers

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ import NavBar from '@/components/nav-bar';
 export default function Layout ({ children }: { children: React.ReactNode }) {
   return (
     <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>{children}</div>
+      <div className='flex-grow md:overflow-y-auto md:p-12'>{children}</div>
       <NavBar />
     </div>
   );
