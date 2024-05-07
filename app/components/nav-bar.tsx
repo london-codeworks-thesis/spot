d55e7b095f0d7da53ca20930fab4c20fc5a02980
Sunny Anter <sunny@veskim.com>
2024-05-07 09:42:12 +0100

    feat: added more functionality to navbar & installed drawer from shadcn

--- app/components/nav-bar.tsx
+++ app/components/nav-bar.tsx
@@ -1,35 +1,67 @@
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import {
   User, Search, Home, MapPinned, Plus,
 } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
+import {
+  Drawer,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerClose,
+  DrawerTrigger,
+} from '@/components/ui/drawer';
 
 export default function NavBar () {
   return (
-    <div className='flex w-full justify-evenly'>
+    <div className='flex w-full justify-evenly pb-3 pt-3 shadow-[0_-1px_24px_0px_rgba(0,0,0,0.1)]'>
       <Link href='/dashboard/home'>
-        <Button>
+        <Button variant='ghost'>
           <Home />
         </Button>
       </Link>
       <Link href='/dashboard/search'>
-        <Button>
+        <Button variant='ghost'>
           <Search />
         </Button>
       </Link>
-      <Link href='/dashboard/add'>
-        <Button>
-          <Plus />
-        </Button>
-      </Link>
+      <Drawer>
+        <DrawerTrigger asChild>
+          <Button variant='ghost'>
+            <Plus />
+          </Button>
+        </DrawerTrigger>
+        <DrawerContent>
+          <div className='mx-auto h-[50vh] w-full max-w-sm'>
+            <DrawerHeader>
+              <DrawerTitle>New Spot</DrawerTitle>
+            </DrawerHeader>
+            <div className='flex w-full justify-center'>
+              <Input
+                className='h-12 w-11/12'
+                placeholder="Search for a spot you'd like to review"
+              />
+            </div>
+            <DrawerFooter className='absolute bottom-1 w-full'>
+              <DrawerClose asChild>
+                <Link href='/dashboard/add' className='w-full'>
+                  <Button className='h-12 w-full'>Review</Button>
+                </Link>
+              </DrawerClose>
+            </DrawerFooter>
+          </div>
+        </DrawerContent>
+      </Drawer>
       <Link href='/dashboard'>
-        <Button>
+        <Button variant='ghost'>
           <MapPinned />
         </Button>
       </Link>
       <Link href='/dashboard/user'>
-        <Button>
+        <Button variant='ghost'>
           <User />
         </Button>
       </Link>
