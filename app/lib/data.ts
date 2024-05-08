0a62e6791b654893d1e53598763c602ccf2f6b4d
Sam Polge <sampolge1@gmail.com>
2024-05-08 10:58:25 +0100

    feat: mockdata for restaurant

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -4,4 +4,45 @@ const markerData = [
   [51.564819, -0.1328599, 3],
 ];
 
-export default markerData;
+const restaurant = {
+  _id: 'ChIJaeMWvsIEdkgRPKGlNOE_vd8',
+  name: 'Sapori',
+  address: '60 Horseferry Rd, London SW1P 2AF, UK',
+  phone: '+442072225488',
+  googleMapsUri: 'https://maps.google.com/?cid=16122112477539705148',
+  priceLevel: 'PRICE_LEVEL_INEXPENSIVE',
+  primaryTypeDisplayName: {
+    text: 'Italian Restaurant',
+  },
+  weekdayDescriptions: [
+    'Monday: 6:30 AM – 10:00 PM',
+    'Tuesday: 6:30 AM – 10:00 PM',
+    'Wednesday: 6:30 AM – 10:00 PM',
+    'Thursday: 6:30 AM – 10:00 PM',
+    'Friday: 6:30 AM – 10:00 PM',
+    'Saturday: 6:30 AM – 4:00 PM',
+    'Sunday: Closed',
+  ],
+  editorialSummary: {
+    text: 'A chalkboard menu of Italian classic dishes & British fare in a relaxed counter-service setting.',
+  },
+  photos: {
+    name: 'places/ChIJaeMWvsIEdkgRPKGlNOE_vd8/photos/ATplDJamJo9y8dEU4r4vEdSm7tssTJZOzNUrAwBrH-w2NWtcbHYU0k5sbmy7h8kz-ENIGmo3WFCaGNe7wMaC9XRNaFQWP7PLxe9XXhRGqaKC2g0xMn_uGayu-272fUxC18csRcDI0O2DRSdclL9zSm4lREmd_ZvxFDHfIefk',
+    widthPx: 4032,
+    heightPx: 3024,
+    authorAttributions: [
+      {
+        displayName: 'Don Kendall',
+        uri: '//maps.google.com/maps/contrib/114259405261741729287',
+        photoUri:
+          '//lh3.googleusercontent.com/a-/ALV-UjU_sO07tXN-7k5DBjCUwq2kATFKNzS6pdn8gIIdYHiiyGJq3FlF=s100-p-k-no-mo',
+      },
+    ],
+  },
+  location: {
+    latitude: 51.4949702,
+    longitude: -0.1277006,
+  },
+};
+
+export default { markerData, restaurant };
