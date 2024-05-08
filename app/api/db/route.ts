face73278aec9372e22748a15006eab89dc67b76
Jason Wong <jasonwong_3@hotmail.com>
2024-05-08 19:36:04 +0100

    feat: prisma schema - restaurant model

--- app/api/db/route.ts
+++ app/api/db/route.ts
@@ -1,27 +1,12 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
 
 export async function GET () {
   const result = await prisma.user.findMany();
   return NextResponse.json(result);
 }
 
-export async function POST() {
+export async function POST () {
   const result = 'post';
   return NextResponse.json(result);
 }
-
-
-// export default async function handle(req, res) {
-//   const { title, content } = req.body;
-
-//   const session = await getSession({ req });
-//   const result = await prisma.post.create({
-//     data: {
-//       title: title,
-//       content: content,
-//       author: { connect: { email: session?.user?.email } },
-//     },
-//   });
-//   res.json(result);
-// }
