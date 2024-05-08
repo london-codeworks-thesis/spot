2267661351f68cff20320a041d1cea7597646d6f
Sunny <153813638+SunnyAnter@users.noreply.github.com>
2024-05-08 11:03:19 +0100

    Style/add review page (#16)
    * feat: installed antd library for rating & styled add page
    
    * feat: styling for add new review page done
    
    * fix: fixed hover color on submit button
    
    * style: changed color coding to black
    
    * fix: changes styling in buttons and added function to prettify numbers
    
    * feat: requested changes made
    
    * feat: changed percentages and wrapped with new div
    
    * [CodeFactor] Apply fixes
    
    ---------
    
    Co-authored-by: Toby Dixon Smith <tobydixonsmith@gmail.com>
    Co-authored-by: codefactor-io <support@codefactor.io>

--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,92 +1,63 @@
 'use client';
 
 import React, { useState } from 'react';
-import { Rate } from 'antd';
 import { PiggyBank, Flame, Cookie } from 'lucide-react';
 import { Card, CardContent } from '@/components/ui/card';
-import { Label } from '@/components/ui/label';
 import { Button } from '@/components/ui/button';
+import RatingCard from '@/components/starNumberCard';
+import StarRatingSystem from '@/components/ratingStars';
 
 export default function Page () {
   const [food, setFood] = useState(2.5);
   const [value, setValue] = useState(2.5);
   const [vibe, setVibe] = useState(2.5);
+
   return (
-    <div className='p-6'>
-      <div className='flex w-full flex-col gap-4'>
+    <div className='flex w-full justify-center'>
+      <div className='flex h-[80vh] w-[90%] flex-col gap-4'>
         <h1 className='pl-6 pt-7 text-3xl font-extrabold'>Add a review</h1>
         <div className='flex w-full justify-center'>
-          <Card className='h-56 w-11/12 bg-gray-50' />
+          <Card className='h-56 w-full bg-gray-50' />
         </div>
-        <div className='flex flex-col gap-2 pl-6'>
+        <div className='flex flex-col gap-2 pb-1 pl-6 '>
           <h1 className='text-3xl font-extrabold'>Itsu</h1>
-          <h4 className='pb-1 text-sm'>60 Horseferry Rd, London, SW1P 2AF</h4>
+          <h4 className='text-sm'>60 Horseferry Rd, London, SW1P 2AF</h4>
         </div>
         <div className='flex w-full flex-col items-center justify-center gap-7'>
-          <Card className='w-11/12 bg-gray-50'>
+          <Card className='w-full bg-gray-50'>
             <CardContent className='flex gap-3 pb-7'>
               <div className='flex w-4/6 flex-col gap-3 pt-6'>
-                <Label className='text-md flex w-20 items-center justify-between'>
-                  Food
-                  {' '}
-                  <Cookie size={20} />
-                </Label>
-                <Rate
-                  value={food}
-                  onChange={(v) => {
-                    setFood(v);
-                    console.log(food);
-                  }}
-                  allowHalf
-                  style={{ color: 'black' }}
+                <StarRatingSystem
+                  Icon={Cookie}
+                  Title='Food'
+                  valueType={food}
+                  setValueType={setFood}
                 />
-                <Label className='text-md flex w-20 items-center justify-between'>
-                  Value
-                  {' '}
-                  <PiggyBank size={20} />
-                </Label>
-                <Rate
-                  value={value}
-                  onChange={(v) => {
-                    setValue(v);
-                    console.log(value);
-                  }}
-                  allowHalf
-                  style={{ color: 'black' }}
+                <StarRatingSystem
+                  Icon={PiggyBank}
+                  Title='Value'
+                  valueType={value}
+                  setValueType={setValue}
                 />
-                <Label className='text-md flex w-20 items-center justify-between'>
-                  Vibe
-                  {' '}
-                  <Flame size={20} className='mb-1' />
-                </Label>
-                <Rate
-                  value={vibe}
-                  onChange={(v) => {
-                    setVibe(v);
-                    console.log(vibe);
-                  }}
-                  allowHalf
-                  style={{ color: 'black' }}
+                <StarRatingSystem
+                  Icon={Flame}
+                  Title='Vibe'
+                  valueType={vibe}
+                  setValueType={setVibe}
                 />
               </div>
-              <div className='flex w-3/6 flex-col items-center gap-2 pt-6'>
-                <Card className='flex h-16 w-24 items-center justify-center'>
-                  <h1 className='text-lg font-bold text-black'>{food}</h1>
-                </Card>
-                <Card className='flex h-16 w-24 items-center justify-center'>
-                  <h1 className='text-lg font-bold text-black'>{value}</h1>
-                </Card>
-                <Card className='flex h-16 w-24 items-center justify-center'>
-                  <h1 className='text-lg font-bold text-black'>{vibe}</h1>
-                </Card>
+              <div className='flex w-[50%] flex-col items-center gap-2 pt-6'>
+                <RatingCard ratingFor={food} />
+                <RatingCard ratingFor={value} />
+                <RatingCard ratingFor={vibe} />
               </div>
             </CardContent>
           </Card>
-          <div className='flex w-11/12 flex-col gap-2'>
-            <Button className='h-12'>Submit</Button>
-            <Button variant='outline' className='h-12'>
+          <div className='mx-2 my-4 flex w-full gap-2'>
+            <Button variant='outline' className='h-12 w-[50%]'>
               Cancel
             </Button>
+            <Button className='h-12 w-[50%]'>Submit</Button>
           </div>
         </div>
       </div>
