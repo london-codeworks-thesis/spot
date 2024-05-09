b880d7a7253d97b6659cfe6382ed7314aa492ecb
TobyDS <tobydixonsmith@gmail.com>
2024-05-09 20:52:35 +0100

    refactor: move map to isolated server component

--- app/dashboard/(map)/page.tsx
+++ app/dashboard/(map)/page.tsx
@@ -1,17 +1,13 @@
-'use client';
-
 import React from 'react';
-import Map, { Marker } from 'react-map-gl';
-import { useSession } from 'next-auth/react';
+import { getServerSession } from 'next-auth';
 import { Input } from '@/components/ui/input';
+import MarkerMap from '@/components/markerMap';
 import data from '../../lib/data';
-import 'mapbox-gl/dist/mapbox-gl.css';
 
-const MAPBOX_TOKEN = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 const coordinates = data.markerData;
 
-export default function Page () {
-  const { data: session } = useSession();
+export default async function Page () {
+  const session = await getServerSession();
   if (session) {
     return (
       <div className='relative h-full w-full'>
@@ -19,25 +15,7 @@ export default function Page () {
           placeholder='Search...'
           className='absolute left-16 top-4 z-10 w-4/6 bg-white p-6'
         />
-        <Map
-          reuseMaps
-          initialViewState={{
-            latitude: 51.4949702,
-            longitude: -0.1277006,
-            zoom: 10,
-          }}
-          mapStyle='mapbox://styles/sampolge/clvwm3wpt01sx01o0e8hcesym'
-          mapboxAccessToken={MAPBOX_TOKEN}
-        >
-          {coordinates.map((item) => (
-            <Marker
-              key={item[2]}
-              latitude={item[0]}
-              longitude={item[1]}
-              color='red'
-            />
-          ))}
-        </Map>
+        <MarkerMap coordinates={coordinates} />
       </div>
     );
   }
