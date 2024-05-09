27402434786f0ae182f00835f11db053948540d9
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 17:01:31 +0100

    feat: prisma - create new review

--- app/api/db/review/route.ts
+++ app/api/db/review/route.ts
@@ -1,12 +1,22 @@
 import prisma from '@/lib/prisma';
 import { NextResponse } from 'next/server';
 
-export async function GET() {
-  const result = await prisma.review.findMany()
+export async function GET () {
+  const result = await prisma.review.findMany();
   return NextResponse.json(result);
 }
 
-export async function POST () {
-  const result = 'review post';
-  return NextResponse.json(result);
+export async function POST (request) {
+  const data = await request.json();
+  const review = await prisma.review.create({
+    data: {
+      user_id: data.userId,
+      restaurant_id: data.restaurantId,
+      rating_food: data.ratingFood,
+      rating_value: data.ratingValue,
+      rating_atmosphere: data.ratingAtmosphere,
+      created_at: new Date(Date.now()),
+    },
+  });
+  return NextResponse.json(review);
 }
