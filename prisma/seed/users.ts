5f090c7dc45168ed432424edb9751ba4f8c30726
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 14:17:03 +0100

    feat: prisma - seed db for user/restaurant/review

---
+++ prisma/seed/users.ts
@@ -0,0 +1,28 @@
+const users = [
+  {
+    username: 'jasonwong',
+    password: 'password',
+    email: 'jason@test.com',
+    first_name: 'jason',
+    last_name: 'wong',
+    image_url: 'placeholder',
+  },
+  {
+    username: 'sunnyanter',
+    password: 'sunnylovessam',
+    email: 'sunnyn@test.com',
+    first_name: 'sunny',
+    last_name: 'anter',
+    image_url: 'placeholder',
+  },
+  {
+    username: 'sampolge',
+    password: 'samlovessunny',
+    email: 'sam@test.com',
+    first_name: 'sam',
+    last_name: 'polge',
+    image_url: 'placeholder',
+  },
+];
+
+export default users;
