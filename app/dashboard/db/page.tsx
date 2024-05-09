eac3e80d03d8a1058509d1a882c4abd35107ee92
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 09:24:41 +0100

    feat: prisma schema - add user, review and follow models

--- app/dashboard/db/page.tsx
+++ app/dashboard/db/page.tsx
@@ -1,15 +1,16 @@
+'use client';
+
 import React from 'react';
 
 async function db () {
+  console.log('clicked');
   const res = await fetch('http://localhost:3000/api/db');
   const data = await res.json();
   console.log('DATA', data);
 }
 
-db();
-
 function Page () {
-  return <p>DB Page</p>;
+  return <p onClick={db}>Get all restaurants</p>
 }
 
 export default Page;
