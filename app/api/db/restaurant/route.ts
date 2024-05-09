3af61610519d04360ea73a3defb722a5dd8970d2
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 14:36:05 +0100

    feat: prisma - minor code clean up

---
+++ app/api/db/restaurant/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+export async function GET () {
+  const result = await prisma.restaurant.findMany();
+  return NextResponse.json(result);
+}
+
+export async function POST () {
+  const result = 'restaurant post';
+  return NextResponse.json(result);
+}
