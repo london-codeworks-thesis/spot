4d4004d4eca1289fb48174493e72cec012d74199
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 14:45:59 +0100

    feat: add db component to test fetches

--- app/dashboard/db/page.tsx
+++ app/dashboard/db/page.tsx
@@ -2,15 +2,39 @@
 
 import React from 'react';
 
-async function db () {
-  console.log('clicked');
-  const res = await fetch('http://localhost:3000/api/db');
+async function getRestaurants () {
+  const res = await fetch('http://localhost:3000/api/db/restaurant');
+  const data = await res.json();
+  console.log('DATA', data);
+}
+
+async function getUsers () {
+  const res = await fetch('http://localhost:3000/api/db/user');
+  const data = await res.json();
+  console.log('DATA', data);
+}
+
+async function getReviews () {
+  const res = await fetch('http://localhost:3000/api/db/review');
+  const data = await res.json();
+  console.log('DATA', data);
+}
+
+async function getFollows () {
+  const res = await fetch('http://localhost:3000/api/db/follow');
   const data = await res.json();
   console.log('DATA', data);
 }
 
 function Page () {
-  return <p onClick={db}>Get all restaurants</p>
+  return (
+    <>
+      <p onClick={getRestaurants}>Get all restaurants</p>
+      <p onClick={getUsers}>Get all users</p>
+      <p onClick={getReviews}>Get all reviews</p>
+      <p onClick={getFollows}>Get all follows</p>
+    </>
+  );
 }
 
 export default Page;
