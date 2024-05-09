88dde1ca9a67246b41b273b533e4167ad8c2f57d
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 17:56:40 +0100

    feat: prisma - get followers/following for specific user

---
+++ app/api/db/follow/[id]/route.ts
@@ -0,0 +1,54 @@
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+// get followers and following for specific user
+export async function GET (req: NextRequest, { params }: { params: { id: string } }) {
+  const userId = params.id;
+  const followers = await prisma.follow.findMany({
+    where: { following_user_id: userId },
+    select: {
+      follower_user: {
+        select: {
+          id: true,
+          username: true,
+          first_name: true,
+          last_name: true,
+          image_url: true,
+        },
+      },
+      is_accepted: true,
+      created_at: true,
+      updated_at: true,
+    },
+  });
+
+  const following = await prisma.follow.findMany({
+    where: { follower_user_id: userId },
+    select: {
+      following_user: {
+        select: {
+          id: true,
+          username: true,
+          first_name: true,
+          last_name: true,
+          image_url: true,
+        },
+      },
+      is_accepted: true,
+      created_at: true,
+      updated_at: true,
+    },
+  });
+
+  const response = {
+    followers,
+    following,
+  };
+
+  return NextResponse.json(response);
+}
+
+export async function POST () {
+  const result = 'follow post';
+  return NextResponse.json(result);
+}
