47e4121577b042884814c41fe5f594dc2a3a0f98
Jason Wong <jasonwong_3@hotmail.com>
2024-05-09 19:26:29 +0100

    feat: prisma - add new restaurant if reviewed and not existing

--- app/api/db/review/route.ts
+++ app/api/db/review/route.ts
@@ -9,15 +9,44 @@ export async function GET () {
 // add new review
 export async function POST (request) {
   const data = await request.json();
+
+  const existingRestaurant = await prisma.restaurant.findUnique({
+    where: {
+      google_id: data.googleId,
+    },
+  });
+
+  let restaurant;
+
+  if (!existingRestaurant) {
+    restaurant = await prisma.restaurant.create({
+      data: {
+        google_id: data.googleId,
+        name: data.name,
+        address: data.address,
+        phone: data.phone,
+        google_maps_uri: data.googleMapsUri,
+        price_level: data.priceLevel,
+        type: data.type,
+        opening_hours: data.openingHours,
+        summary: data.summary,
+        image_url: data.imageUrl,
+        latitude: data.latitude,
+        longitude: data.longitude,
+      },
+    });
+  }
+
   const review = await prisma.review.create({
     data: {
       user_id: data.userId,
-      restaurant_id: data.restaurantId,
+      restaurant_id: existingRestaurant ? existingRestaurant.id : restaurant.id,
       rating_food: data.ratingFood,
       rating_value: data.ratingValue,
       rating_atmosphere: data.ratingAtmosphere,
       created_at: new Date(Date.now()),
     },
   });
+
   return NextResponse.json(review);
 }
