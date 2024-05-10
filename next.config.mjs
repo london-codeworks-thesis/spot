8392d4da0248267da84415f8864a85376957df5c
codefactor-io <support@codefactor.io>
2024-05-10 12:59:32 +0000

    [CodeFactor] Apply fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-        domains: ['images.unsplash.com']
-    }
+    domains: ['images.unsplash.com'],
+  },
 };
 
 export default nextConfig;
