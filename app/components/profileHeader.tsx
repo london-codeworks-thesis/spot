b0a8763ee7cf5c5773a6858c728a134d0e968738
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-05-10 14:05:43 +0100

    Feat/profile (#33)
    * build: add path aliases
    
    * fix: correct paths
    
    * feat: add user page styling
    
    * feat: add user page styling

---
+++ app/components/profileHeader.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+
+type User = {
+  name?: string | null | undefined;
+  email?: string | null | undefined;
+  image?: string | null | undefined;
+};
+
+function ProfileHeader ({
+  name,
+  image,
+}: {
+  name: User['name'];
+  image: User['image'];
+}) {
+  return (
+    <div className='mx-5 flex gap-4'>
+      <Avatar>
+        <AvatarImage src={image as string} width={100} height={100} />
+        <AvatarFallback>TDS</AvatarFallback>
+      </Avatar>
+      <div className='flex flex-col'>
+        <h1 className='text-3xl font-semibold'>{name}</h1>
+        <div className='flex flex-row gap-8 font-semibold'>
+          <div className='flex flex-col items-center'>
+            <p>20</p>
+            <p className='text-xs'>Followers</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <p>30</p>
+            <p className='text-xs'>Following</p>
+          </div>
+          <div className='flex flex-col items-center'>
+            <p>10</p>
+            <p className='text-xs'>Reviews</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileHeader;
--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { getServerSession } from 'next-auth';
+import ProfileHeader from '@components/profileHeader';
+import { Settings } from 'lucide-react';
+import type { User } from '@/types/user';
 
-export default function Page () {
-  return <p>User Page</p>;
+export default async function Page () {
+  const session = await getServerSession();
+  if (!session) {
+    // TODO: handle error
+    return null;
+  }
+  const user = session?.user as User;
+  console.log(user.image);
+  return (
+    <div className='mx-5 my-5 flex flex-col gap-8'>
+      <div className='flex justify-end'>
+        <Settings size={28} strokeWidth={2.25} />
+      </div>
+      <ProfileHeader name={user.name} image={user.image} />
+    </div>
+  );
 }
