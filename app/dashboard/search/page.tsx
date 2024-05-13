d421c92da2c441d532438ecfa188363612800ac4
Jason Wong <jasonwong_3@hotmail.com>
2024-05-13 09:59:04 +0100

    refactor: removed temp arr

--- app/dashboard/search/page.tsx
+++ app/dashboard/search/page.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from '@/components/ui/command';
 
 export default function Page () {
-  return <p>Search Page</p>;
+  return (
+    <div>
+      <div className='flex w-full justify-center pt-[10%]'>
+        <div className='w-[90%] flex-col'>
+          <Command>
+            <CommandInput placeholder='Type a command or search...' />
+            <CommandList>
+              <CommandEmpty>No results found.</CommandEmpty>
+              <CommandGroup heading='Suggestions'>
+                <CommandItem>Calendar</CommandItem>
+                <CommandItem>Search Emoji</CommandItem>
+                <CommandItem>Calculator</CommandItem>
+              </CommandGroup>
+            </CommandList>
+          </Command>
+        </div>
+      </div>
+    </div>
+  );
 }
