f5e09ac3bf86c4ecda4c02fcb5593ec4ab2b48ea
TobyDS <tobydixonsmith@gmail.com>
2024-05-14 13:53:19 +0100

    feat(profile): add map

--- app/dashboard/user/page.tsx
+++ app/dashboard/user/page.tsx
@@ -1,29 +1,44 @@
-// page.tsx
 import React from 'react';
 import ProfileHeader from '@components/profileHeader';
 import Settings from '@components/settings';
-import type { User } from '@/types/user';
+import MarkerMap from '@components/markerMap';
 import { getSession } from '@/hooks/getSession';
+import { getUserById } from '@/lib/userService';
 
 export default async function Page () {
   const session = await getSession();
-  const user = session?.user as User;
 
-  // Ensure user data is available
-  if (!user) {
+  if (!session?.user?.id) {
     return <div>Loading...</div>;
   }
 
+  const user = await getUserById(session.user.id);
+
+  if (!user) {
+    return <div>User not found</div>;
+  }
+
   return (
-    <div className='mx-5 my-5 flex flex-col gap-4'>
-      <div className='flex justify-end'>
-        <Settings />
+    <div className='mx-5 my-5 flex flex-col gap-8'>
+      <div className='flex flex-col gap-4'>
+        <div className='flex justify-end'>
+          <Settings />
+        </div>
+        <ProfileHeader
+          first_name={user.first_name || 'First Name'}
+          last_name={user.last_name || 'Last Name'}
+          image={user.image || ''}
+        />
       </div>
-      <ProfileHeader
-        first_name={user.first_name || 'First Name'}
-        last_name={user.last_name || 'Last Name'}
-        image={user.image || ''}
-      />
+      <div>
+        <h2 className='text-2xl font-semibold'>Review Map</h2>
+        <div className='h-[30vh] w-full'>
+          <MarkerMap data={user.reviews} />
+        </div>
+      </div>
+      <div>
+        <h2 className='text-2xl font-semibold'> Recent Reviews</h2>
+      </div>
     </div>
   );
 }
---
+++ app/lib/userService.ts
@@ -0,0 +1,48 @@
+import prisma from './prisma';
+
+export async function getUserById (userId: string) {
+  if (!userId) {
+    throw new Error('User ID is required');
+  }
+
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    select: {
+      id: true,
+      first_name: true,
+      last_name: true,
+      image: true,
+      reviews: {
+        select: {
+          restaurant: {
+            select: {
+              id: true,
+              name: true,
+              image_url: true,
+              reviews: true,
+              latitude: true,
+              longitude: true,
+            },
+          },
+          rating_food: true,
+          rating_value: true,
+          rating_atmosphere: true,
+          created_at: true,
+        },
+        orderBy: {
+          created_at: 'desc',
+        },
+      },
+      _count: {
+        select: {
+          reviews: true,
+          followers: true,
+          following: true,
+        },
+      },
+    },
+  });
+  return user;
+}
