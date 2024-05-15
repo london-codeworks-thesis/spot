087e1ddc49f4902ac94ea29eb35c83a27e59af0f
TobyDS <tobydixonsmith@gmail.com>
2024-05-15 01:56:02 +0100

    feat: update add review to use server side

---
+++ app/api/restaurant/[id]/reviews/review/route.ts
@@ -0,0 +1,54 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+
+export async function GET () {
+  const result = await prisma.review.findMany();
+  return NextResponse.json(result);
+}
+
+// add new review
+export async function POST (req: NextRequest) {
+  const data = await req.json();
+
+  const existingRestaurant = await prisma.restaurant.findUnique({
+    where: {
+      google_id: data.google_id,
+    },
+  });
+
+  let restaurant;
+
+  if (!existingRestaurant) {
+    restaurant = await prisma.restaurant.create({
+      data: {
+        google_id: data.google_id,
+        name: data.name,
+        address: data.address,
+        phone: data.phone,
+        google_maps_uri: data.google_maps_uri,
+        price_level: data.price_level,
+        type: data.type,
+        opening_hours: data.opening_hours,
+        summary: data.summary,
+        image_url: data.image_url,
+        latitude: data.latitude,
+        longitude: data.longitude,
+      },
+    });
+  }
+
+  const review = await prisma.review.create({
+    data: {
+      user_id: data.userId,
+      restaurant_id: existingRestaurant
+        ? existingRestaurant.id
+        : restaurant!.id,
+      rating_food: data.rating_food,
+      rating_value: data.rating_value,
+      rating_atmosphere: data.rating_atmosphere,
+      created_at: new Date(Date.now()),
+    },
+  });
+
+  return NextResponse.json(review);
+}
--- app/dashboard/add/page.tsx
+++ app/dashboard/add/page.tsx
@@ -1,14 +1,8 @@
-'use client';
-
-import React, { useState } from 'react';
-import { PiggyBank, Flame, Cookie } from 'lucide-react';
-import { Card, CardContent } from '@/components/ui/card';
-import { Button } from '@/components/ui/button';
-import RatingCard from '@/components/starNumberCard';
-import Link from 'next/link';
-import StarRatingSystem from '@/components/ratingStars';
+import React from 'react';
+import { redirect } from 'next/navigation';
+import { getSession } from '@/hooks/getSession';
 import { User } from '@/types/user';
-import { useSession } from 'next-auth/react';
+import ReviewForm from '@/components/reviewForm';
 
 interface PageProps {
   searchParams: {
@@ -17,52 +11,23 @@ interface PageProps {
   };
 }
 
-export default function Page ({ searchParams }: PageProps) {
-  const [food, setFood] = useState(2.5);
-  const [value, setValue] = useState(2.5);
-  const [vibe, setVibe] = useState(2.5);
+export default async function Page ({ searchParams }: PageProps) {
+  const session = await getSession();
+  const user = session?.user as User | null;
+
+  if (!user) {
+    redirect('/');
+  }
+
   const restaurant = JSON.parse(searchParams.restaurant);
   const img = JSON.parse(searchParams.imgSource);
-  const { data } = useSession();
-
-  if (!data) {
-    // TODO: handle error
-    return null;
-  }
-  const user = data?.user as User;
-
-  async function postReview () {
-    try {
-      const body = {
-        ...restaurant,
-        rating_food: food,
-        rating_value: value,
-        rating_atmosphere: vibe,
-        userId: user.id,
-      };
-      const res = await fetch('http://localhost:3000/api/db/review', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(body),
-      });
-      console.log('new mcdo', res.json());
-      if (!res.ok) {
-        throw new Error(`Failed to post review: ${res.status}`);
-      }
-    } catch (error) {
-      console.error('Error sending reviews', error);
-      throw error;
-    }
-  }
 
   return (
     <div className='flex h-full w-full justify-center'>
       <div className='flex h-full w-[90%] flex-col justify-around'>
         <h1 className='pl-6 pt-7 text-3xl font-extrabold'>Add a review</h1>
         <div className='flex w-full justify-center'>
-          <Card
+          <div
             className='h-56 w-full bg-cover bg-center bg-no-repeat'
             style={{ backgroundImage: `url(${img})` }}
           />
@@ -71,47 +36,7 @@ export default function Page ({ searchParams }: PageProps) {
           <h1 className='text-3xl font-extrabold'>{restaurant.name}</h1>
           <h4 className='text-sm'>{restaurant.address}</h4>
         </div>
-        <div className='flex w-full flex-col items-center justify-center gap-7'>
-          <Card className='w-full bg-gray-50'>
-            <CardContent className='flex gap-3 pb-7'>
-              <div className='flex w-2/3 flex-col gap-3 pt-6'>
-                <StarRatingSystem
-                  Icon={Cookie}
-                  Title='Food'
-                  valueType={food}
-                  setValueType={setFood}
-                />
-                <StarRatingSystem
-                  Icon={PiggyBank}
-                  Title='Value'
-                  valueType={value}
-                  setValueType={setValue}
-                />
-                <StarRatingSystem
-                  Icon={Flame}
-                  Title='Vibe'
-                  valueType={vibe}
-                  setValueType={setVibe}
-                />
-              </div>
-              <div className='flex w-[50%] flex-col items-center gap-2 pt-6'>
-                <RatingCard ratingFor={food} />
-                <RatingCard ratingFor={value} />
-                <RatingCard ratingFor={vibe} />
-              </div>
-            </CardContent>
-          </Card>
-          <div className='mx-2 my-4 flex w-full gap-2'>
-            <Button variant='outline' className='h-12 w-[50%]'>
-              Cancel
-            </Button>
-            <Link href='/dashboard' className='w-[50%]'>
-              <Button className='h-12 w-full' onClick={() => postReview()}>
-                Submit
-              </Button>
-            </Link>
-          </div>
-        </div>
+        <ReviewForm restaurant={restaurant} user={user} />
       </div>
     </div>
   );
