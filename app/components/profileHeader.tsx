4c28a1e3a434ff132f84238434ae0789acb4bc67
TobyDS <tobydixonsmith@gmail.com>
2024-05-15 12:52:42 +0100

    feat(profile): add fallback for profile images

--- app/components/profileHeader.tsx
+++ app/components/profileHeader.tsx
@@ -16,16 +16,31 @@ function ProfileHeader ({
   following: number;
   reviews: number;
 }) {
+  // To account for multiword names
+  const getInitials = (name: string) => name
+    .split(' ')
+    .map((word) => word[0])
+    .join('')
+    .toUpperCase();
+
+  const initials = `${getInitials(first_name)}${getInitials(last_name)}`;
+
   return (
     <div className='flex items-center gap-4'>
-      <Image
-        src={image as string}
-        alt='Profile Picture'
-        width={100}
-        height={100}
-        className='aspect-square rounded-full object-cover'
-        priority
-      />
+      {image ? (
+        <Image
+          src={image as string}
+          alt='Profile Picture'
+          width={100}
+          height={100}
+          className='aspect-square rounded-full object-cover'
+          priority
+        />
+      ) : (
+        <div className='flex h-24 w-24 items-center justify-center rounded-full bg-gray-500'>
+          <span className='text-3xl font-semibold text-white'>{initials}</span>
+        </div>
+      )}
       <div className='flex-grow-1 flex flex-grow flex-col'>
         <h1 className='text-3xl font-semibold'>{first_name}</h1>
         <p className='text-3xl font-semibold'>{last_name}</p>
