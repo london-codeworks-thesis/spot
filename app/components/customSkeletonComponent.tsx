18300e2525fb0fccc94f05a96a4c40c8dc5283af
Sam Polge <sampolge1@gmail.com>
2024-05-16 08:32:28 +0100

    style: small change (#67)

--- app/components/customSkeletonComponent.tsx
+++ app/components/customSkeletonComponent.tsx
@@ -4,7 +4,7 @@ import { Skeleton } from './ui/skeleton';
 export default function CustomSkeletonComponent () {
   return (
     <div className='flex flex-col space-y-3 pb-5'>
-      <Skeleton className='h-[80px] w-[150px] rounded-xl' />
+      <Skeleton className='h-[110px] w-[150px] rounded-xl' />
       <div className='flex items-center space-x-4'>
         <Skeleton className='h-8 w-8 rounded-full' />
         <div className='space-y-2'>
