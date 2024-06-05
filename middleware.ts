f160b92aa91c6427a8539c28ab90e5e54b87fca1
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-06-06 00:51:35 +0100

    feat(auth): add route protection using middleware

--- middleware.ts
+++ middleware.ts
@@ -1,23 +1,17 @@
-// import { NextResponse, NextRequest } from 'next/server';
-// import { auth } from '@/auth';
+import { auth } from '@/auth';
 
-// export async function middleware (req: NextRequest) {
-//   try {
-//     const session = await auth();
-//     console.log('session', session);
+// eslint-disable-next-line consistent-return
+export default auth((req) => {
+  if (
+    !req.auth
+    && req.nextUrl.pathname !== '/login'
+    && req.nextUrl.pathname !== '/'
+  ) {
+    const newUrl = new URL('/', req.nextUrl.origin);
+    return Response.redirect(newUrl);
+  }
+});
 
-//     if (!session || !session.user?.id) {
-//       throw new Error('Session is undefined or id is missing');
-//     }
-
-//     return NextResponse.next();
-//   } catch (error) {
-//     // console.error(error);
-//     // Redirect to login page or throw error
-//     return NextResponse.rewrite(new URL('/', req.url));
-//   }
-// }
-// export const config = {
-//   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
-// };
-export { auth as middleware } from '@/auth';
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
+};
