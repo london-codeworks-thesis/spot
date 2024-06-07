7b67020ef45a49aedf9b624520027fd05a85c7b6
Toby Dixon Smith <tobydixonsmith@gmail.com>
2024-06-08 00:07:30 +0100

    feat: improve home loading skeleton

--- app/dashboard/home/loading.tsx
+++ app/dashboard/home/loading.tsx
@@ -3,20 +3,20 @@ import CustomSkeletonComponent from '@/components/customSkeletonComponent';
 
 async function Page () {
   return (
-    <div className='flex w-full justify-center pt-[10%]'>
-      <div className='w-[90%] flex-col'>
+    <div className='flex h-full w-full flex-col justify-center pt-8'>
+      <div className='mx-auto flex h-full w-[90%] flex-col'>
         <div className='mb-4 text-2xl font-extrabold'>
           <h1>For You</h1>
         </div>
-        <div className='flex w-[100%] flex-row gap-3'>
-          <div className='flex w-[50%] flex-col overflow-hidden'>
+        <div className='relative grid flex-1 grid-cols-2 gap-3 overflow-hidden'>
+          <div className='grid gap-3'>
             <CustomSkeletonComponent />
             <CustomSkeletonComponent />
             <CustomSkeletonComponent />
             <CustomSkeletonComponent />
             <CustomSkeletonComponent />
           </div>
-          <div className='flex w-[50%] flex-col overflow-hidden'>
+          <div className='grid gap-1'>
             <CustomSkeletonComponent />
             <CustomSkeletonComponent />
             <CustomSkeletonComponent />
--- components/customSkeletonComponent.tsx
+++ components/customSkeletonComponent.tsx
@@ -3,13 +3,13 @@ import { Skeleton } from '@/ui/skeleton';
 
 export default function CustomSkeletonComponent () {
   return (
-    <div className='flex flex-col space-y-3 pb-5'>
-      <Skeleton className='h-[110px] w-[150px] rounded-xl' />
-      <div className='flex items-center space-x-4'>
-        <Skeleton className='h-8 w-8 rounded-full' />
-        <div className='space-y-2'>
-          <Skeleton className='h-2 w-[70px]' />
-          <Skeleton className='h-2 w-[70px]' />
+    <div>
+      <Skeleton className='h-[130px] w-full rounded-lg' />
+      <div className='m-1 flex items-center p-1'>
+        <Skeleton className='mr-1 h-[35px] w-[35px] rounded-full' />
+        <div className='ml-0.5 flex flex-1 flex-col space-y-2'>
+          <Skeleton className='h-3 w-[90%]' />
+          <Skeleton className='h-3 w-[70%]' />
         </div>
       </div>
     </div>
